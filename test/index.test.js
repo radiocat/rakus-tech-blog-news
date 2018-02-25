f5076cfe9ffd84483957b2485b8e51dd8181b619
radiocat <radiocat.app@gmail.com>
2018-02-26 00:33:22 +0900

    テストを書くためにgulp,mochaなどを追加

---
+++ test/index.test.js
@@ -0,0 +1,9 @@
+var assert = require("assert")
+
+describe('router', function(){
+  it('Hello mocha', function(){
+    var msg = 'mocha';
+    assert.equal('Hello ' + msg, 'Hello mocha');
+  })
+})
+
