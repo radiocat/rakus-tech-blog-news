7d9ef7b22ee9decb871d9338d609c6ca152f9d2d
radiocat <radiocat.app@gmail.com>
2018-03-03 15:57:13 +0900

    ユニットテスト環境の整備
    - gulp導入
    - mochaの導入、その他必要なライブラリの追加
    - カバレッジ計測用のgulp-istanbulを導入

--- index.js
+++ index.js
@@ -13,9 +13,13 @@ app.use(bodyParser.json());
 app.use(express.static('public'));
 app.set('port', (process.env.PORT || 5000));
 
-app.listen(app.get('port'), function() {
+const server = app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
+
+exports.closeServer = function(){
+  server.close();
+};
 
 const sendResponse = function(response, resultObject){
   response.setHeader("Content-Type", "application/json");
--- test/index.test.js
+++ test/index.test.js
@@ -1,9 +1,33 @@
-var assert = require("assert")
+var index = require('../index.js');
+var assert = require('assert');
+var request = require('supertest');
+var host = 'http://localhost:5000';
 
-describe('router', function(){
+describe('Test of Test', function(){
   it('Hello mocha', function(){
     var msg = 'mocha';
     assert.equal('Hello ' + msg, 'Hello mocha');
-  })
-})
+  });
+});
+
+describe('Server Test', function(){
+  describe('/', function(){
+    it('Get Access Error', function(){
+      var status = 400;
+      request(host)
+        .get('/')
+        .expect(status, '', function(err) {
+           console.log('Get Error Tests');
+           done(err);
+        });
+    });
+  });
+
+  after(function() {
+    index.closeServer();
+  });
+
+});
+
+
 
